5b8cb354ba32887728d2ccd8b6442ac46ece4b58
Rohit Kamble <krohit.kamble@gmail.com>
2024-06-18 12:18:52 +0530

    changed app code

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!, This is my first AWS Devops Pipeline'
+    return 'Hello, world!, This is my first AWS Devops Pipeline with Python App'
 
 if __name__ == '__main__':
     app.run()
