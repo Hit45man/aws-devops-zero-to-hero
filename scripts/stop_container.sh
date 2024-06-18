b801b860f28ec2faff0b5ec2d0f97087a73fdbae
Rohit Kamble <krohit.kamble@gmail.com>
2024-06-18 13:12:13 +0530

    changed entire script

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
+# set -e
 
-# Stop the running container (if any)
-containerid = docker ps | awk -F " " '{print $1}'
-docker rm -f $containerid
+# # Stop the running container (if any)
+# containerid = docker ps | awk -F " " '{print $1}'
+# docker rm -f $containerid
+
+#!/bin/bash
+
+# Stop all running Docker containers
+docker stop $(docker ps -aq)
+
+# Remove all stopped Docker containers
+docker rm $(docker ps -aq -f status=exited)
+
+echo "All running Docker containers stopped and removed."
